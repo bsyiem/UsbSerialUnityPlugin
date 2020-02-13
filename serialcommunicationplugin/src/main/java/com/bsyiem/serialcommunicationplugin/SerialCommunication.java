b029822c762b084154fff228beaa5c54059ac123
Brandon Victor Syiem <bsyiem92@gmail.com>
2020-02-13 15:33:42 +1100

    working communication

--- serialcommunicationplugin/src/main/java/com/bsyiem/serialcommunicationplugin/SerialCommunication.java
+++ serialcommunicationplugin/src/main/java/com/bsyiem/serialcommunicationplugin/SerialCommunication.java
@@ -1,6 +1,8 @@
 package com.bsyiem.serialcommunicationplugin;
 
 import android.app.Fragment;
+import android.content.Context;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.physicaloid.lib.Physicaloid;
@@ -25,22 +27,54 @@ public class SerialCommunication extends Fragment {
         this.physicaloid = new Physicaloid(this.getContext());
         this.physicaloid.setBaudrate(9600);
         Toast.makeText(this.getContext(),"physicaloid created", Toast.LENGTH_LONG).show();
+        Log.d(TAG,"debug test");
     }
 
     public void openConnection(){
         if(physicaloid.open()){
+            Log.d(TAG,"port open");
             // read listener, When new data is received from Arduino add it to Text view
             physicaloid.addReadListener(new ReadLisener() {
                 @Override
                 public void onRead(int size) {
+                    Log.d(TAG,"received info");
                     byte[] buf = new byte[size];
                     physicaloid.read(buf, size);
-                    Toast.makeText(INSTANCE.getContext(),new String(buf),Toast.LENGTH_LONG).show();
+
+                    DisplayTextRunnable myRunnable = new DisplayTextRunnable();
+                    myRunnable.setContext(INSTANCE.getContext());
+                    myRunnable.setText(new String(buf));
+
+                    UnityPlayer.currentActivity.runOnUiThread(myRunnable);
+//                    Toast.makeText(INSTANCE.getContext(),new String(buf),Toast.LENGTH_LONG).show();
                 }
             });
         }else {
             //Error while connecting
             Toast.makeText(INSTANCE.getContext(), "Cannot open", Toast.LENGTH_LONG).show();
         }
+    }
+
+    private void displayToast(String str){
+        Toast.makeText(INSTANCE.getContext(), str, Toast.LENGTH_LONG).show();
+    }
+}
+
+class DisplayTextRunnable implements Runnable{
+
+    String text;
+    Context context;
+
+    public void setContext(Context context) {
+        this.context = context;
+    }
+
+    public void setText(String text){
+        this.text = text;
+    }
+
+    @Override
+    public void run() {
+        Toast.makeText(this.context, this.text, Toast.LENGTH_LONG).show();
     }
 }
